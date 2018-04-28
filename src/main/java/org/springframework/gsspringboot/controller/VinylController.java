ae6d4b3f6d125c9886eb843df5499a56f4857d0f
irwinjuice <irwinjuice@gmail.com>
2018-04-28 11:22:46 +0300

    Images methods has been added

--- src/main/java/org/springframework/gsspringboot/controller/VinylController.java
+++ src/main/java/org/springframework/gsspringboot/controller/VinylController.java
@@ -6,13 +6,24 @@ import org.springframework.gsspringboot.service.VinylService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.validation.Valid;
 
 @Controller
 public class VinylController {
+
+    private final String UPLOAD_FOLDER =
+            "D:\\Documents\\Projects\\ShopOnSB\\src\\main\\resources\\static\\images\\vinil\\imgs_bd_vinil\\";
+    private final String DB_PATH =
+            "images/vinil/imgs_bd_vinil/";
 
     private VinylService vinylService;
 
@@ -30,8 +41,19 @@ public class VinylController {
     @RequestMapping(value = "/completeAP", method = RequestMethod.POST)
     public ModelAndView processAddNewVinyl(ModelAndView modelAndView,
                                            @Valid Vinyl vinyl,
-                                           /*BindingResult bindingResult,*/
-                                           HttpServletRequest request){
+                                           /*BindingResult bindingResult,
+                                           HttpServletRequest request*/
+                                           @RequestParam("file")MultipartFile file){
+        byte[] bytes;
+        try {
+            bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        vinyl.setImageLink(DB_PATH + file.getOriginalFilename());
         vinylService.saveVinyl(vinyl);
         modelAndView.setViewName("completeApp/completeAP");
         return modelAndView;
